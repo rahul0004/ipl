e6e3b561d266ce936da6f4982437140a42a0f2d6
Vilas Kasar <rahul.vilas.kasar@accenture.com>
2018-04-04 22:58:03 +0530

    added db queries

---
+++ db/04042018/ipl_2018_ipl_players_bio.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: ipl_2018
+-- ------------------------------------------------------
+-- Server version	5.7.21-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ipl_players_bio`
+--
+
+DROP TABLE IF EXISTS `ipl_players_bio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ipl_players_bio` (
+  `ipl_players_bio_team_id` int(11) NOT NULL,
+  `ipl_players_bio_id` varchar(45) NOT NULL,
+  `ipl_players_bio_country` varchar(45) NOT NULL,
+  `ipl_players_bio_profile` varchar(3000) NOT NULL,
+  `ipl_players_bio_img_url` varchar(5000) DEFAULT NULL,
+  `ipl_players_bio_bat_style` varchar(200) DEFAULT NULL,
+  `ipl_players_bio_bowl_style` varchar(200) DEFAULT NULL,
+  `ipl_players_bio_major_teams` varchar(5000) DEFAULT NULL,
+  `ipl_players_bio_full_name` varchar(200) DEFAULT NULL,
+  `ipl_players_bio_name` varchar(200) DEFAULT NULL,
+  `ipl_players_bio_category` varchar(45) NOT NULL,
+  `ipl_players_bio_playing_role` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`ipl_players_bio_team_id`,`ipl_players_bio_id`),
+  CONSTRAINT `ipl_team_players_N_ipl_players_bio_pid_FK` FOREIGN KEY (`ipl_players_bio_team_id`) REFERENCES `ipl_teams_players` (`ipl_teams_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-04-04 22:52:10
---
+++ db/04042018/ipl_2018_ipl_user_scores.sql
@@ -0,0 +1,43 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: ipl_2018
+-- ------------------------------------------------------
+-- Server version	5.7.21-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ipl_user_scores`
+--
+
+DROP TABLE IF EXISTS `ipl_user_scores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ipl_user_scores` (
+  `ipl_user_scores_un` varchar(100) NOT NULL,
+  `ipl_user_scores_score` int(11) NOT NULL,
+  `ipl_user_scores_tstamp` datetime NOT NULL,
+  PRIMARY KEY (`ipl_user_scores_un`),
+  CONSTRAINT `ipl_user_scores_ipl_users_cred_FK` FOREIGN KEY (`ipl_user_scores_un`) REFERENCES `ipl_users_cred` (`ipl_users_cred_username`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-04-04 22:52:11
